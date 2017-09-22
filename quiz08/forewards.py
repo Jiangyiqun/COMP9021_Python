b15f846899cd5ba384593dbbaa24c924de2f10ff
Jack Jiang <jackjyq@outlook.com>
2017-09-22 23:04:09 +1000

    quiz8 partly

---
+++ quiz08/forewards.py
@@ -0,0 +1,58 @@
+from stack_adt import *
+
+
+def forewards(grid, target, path, current_sum):
+    '''
+    Return True if current_sum is less or equal to target
+    Return False if no way to go
+    '''
+    change_direction = {(-1, 0): (0, 1), (0, 1): (1, 0), (1, 0): (0, -1), (0, -1): (-1, 0)}
+    # initialize direction
+    present_x, present_y = path.pop()
+    try:
+        previous_x, previous_y = path.peek()
+        direction_x, direction_y = present_x - previous_x, present_y - previous_y
+    except EmptyStackError:
+        direction_x, direction_y = -1, 0
+    path.push((present_x, present_y))
+    # calculate next point and check its validaty
+    for _ in range(4):
+        next_x, next_y = present_x + direction_x, present_y + direction_y
+        if next_x >= 10 or next_x <0 \
+                or next_y >= 10 or next_y <0 \
+                or (next_x, next_y) in path._data:  # if next point is out of board
+            direction_x, direction_y = change_direction[direction_x, direction_y]
+        elif current_sum + grid[next_x][next_y] > target:                   # if next point causes sum out of target
+            direction_x, direction_y = change_direction[direction_x, direction_y]
+        else:
+            return (next_x, next_y)
+    return (present_x, present_y)
+
+
+if __name__ == "__main__":
+    # initialize global value
+    change_direction = {(-1, 0): (0, 1), (0, 1): (1, 0), (1, 0): (0, -1), (0, -1): (-1, 0)}
+    grid = [[6, 6, 0, 4, 8, 7, 6, 4, 7, 5],\
+            [9, 3, 8, 2, 4, 2, 1, 9, 4, 8],\
+            [9, 2, 4, 1, 1, 5, 7, 8, 1, 5],\
+            [6, 5, 9, 3, 8, 7, 7, 8, 4, 0],\
+            [8, 0, 1, 6, 0, 9, 7, 5, 3, 5],\
+            [1, 3, 9, 3, 3, 2, 8, 7, 1, 1],\
+            [5, 8, 7, 1, 4, 8, 4, 1, 8, 5],\
+            [8, 3, 9, 8, 9, 4, 7, 1, 9, 6],\
+            [5, 9, 3, 4, 2, 3, 2, 0, 9, 4],\
+            [7, 1, 1, 2, 2, 0, 1, 8, 6, 8]]   
+    target = 20
+    path = Stack()
+    path.push((2, 7))
+    current_sum = 8
+    for _ in range(4):
+        (return_x, return_y) = forewards(grid, target, path, current_sum)
+        if (return_x, return_y) == path.peek():
+            print(False)
+        else:
+            print(True)
+            current_sum += grid[return_x][return_y]
+            path.push((return_x, return_y))
+        print(current_sum)
+        print(path.peek())
---
+++ quiz08/quiz_8.py
@@ -0,0 +1,63 @@
+# Randomly fills a grid of size 10 x 10 with digits between 0
+# and bound - 1, with bound provided by the user.
+# Given a point P of coordinates (x, y) and an integer "target"
+# also all provided by the user, finds a path starting from P,
+# moving either horizontally or vertically, in either direction,
+# so that the numbers in the visited cells add up to "target".
+# The grid is explored in a depth-first manner, first trying to move north,
+# always trying to keep the current direction,
+# and if that does not work turning in a clockwise manner.
+#
+# Written by Eric Martin for COMP9021
+
+
+import sys
+from random import seed, randrange
+
+from stack_adt import *
+from forewards import *
+
+
+def display_grid():
+    for i in range(len(grid)):
+        print('   ', ' '.join(str(grid[i][j]) for j in range(len(grid[0]))))
+
+
+def explore_depth_first(x, y, target):
+    '''
+    try test case: 0 10 3 3 20
+    '''
+    path = Stack()
+    path.push((x, y))
+    current_sum = grid[x][y]
+    if current_sum == target:
+        return path._data
+    while current_sum != target:
+        (return_x, return_y) = forewards(grid, target, path, current_sum)
+        if (return_x, return_y) == path.peek():
+            return []
+        else:
+            current_sum += grid[return_x][return_y]
+            path.push((return_x, return_y))
+    return path._data
+
+
+try:
+    for_seed, bound, x, y, target = [int(x) for x in input('Enter five integers: ').split()]
+    if bound < 1 or x not in range(10) or y not in range(10) or target < 0:
+        raise ValueError
+except ValueError:
+    print('Incorrect input, giving up.')
+    sys.exit()
+seed(for_seed)
+grid = [[randrange(bound) for _ in range(10)] for _ in range(10)]
+# print(grid)
+print('Here is the grid that has been generated:')
+display_grid()
+path = explore_depth_first(x, y, target)
+if not path:
+    print(f'There is no way to get a sum of {target} starting from ({x}, {y})')
+else:
+    print('With North as initial direction, and exploring the space clockwise,')
+    print(f'the path yielding a sum of {target} starting from ({x}, {y}) is:')
+    print(path)
